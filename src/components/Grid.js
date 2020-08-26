4cde726102718b5ed93b1b9b8c856c6c06f970f8
Jimmique <jimmique88@gmail.com>
2020-08-25 23:31:27 -0700

    Created Cell and cell styling, also added toggle functionality, and refactored the gri d

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import Cell from "./Cell.js";
+import { makeCells, getGridOffset } from "../helpers.js";
 
-function Grid(props) {
-  const { gridSize, cellSize } = props;
+function Grid({ rows, cols, cellSize, grid }, ref) {
+  const [cells, setCells] = useState([]);
+
+  const handleClick = (e) => {
+    const gridOffset = getGridOffset(ref);
+    // MouseEvent.clientX/Y returns
+    // The horizontal and vertical coordinates within the App component
+    // where the mouse was clicked
+
+    // How many pixels from the left of the Grid the mouse click event occured
+    const offsetX = e.clientX - gridOffset.x;
+
+    // How many pixels from the top of the Grid the mouse click event occured
+    const offsetY = e.clientY - gridOffset.y;
+
+    // Get the x and y indexes of the cell that was clicked
+    const x = Math.floor(offsetX / cellSize);
+    const y = Math.floor(offsetY / cellSize);
+
+    // If x and y are within bounds of the grid, toggle the state of that cell
+    if (x >= 0 && x <= cols && y >= 0 && y <= rows) {
+      grid[y][x] = !grid[y][x];
+    }
+
+    // Update the state of the cells based on the updated grid
+    setCells(makeCells(rows, cols, grid));
+  };
+
   return (
     <section>
       <div
+        onClick={handleClick}
+        ref={ref}
         className="grid"
         style={{
-          width: gridSize.width,
-          height: gridSize.height,
+          width: cols * cellSize,
+          height: rows * cellSize,
           backgroundSize: `${cellSize}px ${cellSize}px`,
         }}
-      ></div>
+      >
+        {cells.map((cell) => (
+          <Cell
+            className="cell"
+            cellSize={cellSize}
+            x={cell.x}
+            y={cell.y}
+            key={`${cell.x}, ${cell.y}`}
+          />
+        ))}
+      </div>
     </section>
   );
 }
 
-export default Grid;
+const forwardedGrid = React.forwardRef(Grid);
+
+export default forwardedGrid;
