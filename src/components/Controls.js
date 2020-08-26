44e677f54daa30a3916e530dc649074277881ff4
Jimmique <jimmique88@gmail.com>
2020-08-26 00:51:27 -0700

    Created controls with start/stop and next functionality, also added a generation counter to the app

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,9 +1,98 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { makeEmptyGrid, getNeighborCount } from "../helpers.js";
+import GridContext from "../contexts/gridContext.js";
+import CellContext from "../contexts/cellContext.js";
+import { getLiveCells } from "../helpers.js";
 
-function Controls() {
+function Controls({ isRunning, setIsRunning, rows, cols, setGeneration }) {
+  const { grid, setGrid } = useContext(GridContext);
+  const { setLiveCells } = useContext(CellContext);
+  const [timeoutId, setTimeoutId] = useState(null);
+
+  const start = () => {
+    setIsRunning(true);
+    makeNewGenerations(grid, cols, rows);
+  };
+
+  const stop = () => {
+    clearTimeout(timeoutId);
+    setTimeoutId(null);
+    setIsRunning(false);
+  };
+
+  const next = () => {
+    makeGenerationOnce(grid, cols, rows);
+  };
+
+  const makeGenerationOnce = (grid, cols, rows) => {
+    const newGrid = makeEmptyGrid(rows, cols);
+
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        let neighbors = getNeighborCount(grid, x, y, cols, rows);
+
+        if (grid[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newGrid[y][x] = true;
+          } else {
+            newGrid[y][x] = false;
+          }
+        } else {
+          if (!grid[y][x] && neighbors === 3) {
+            newGrid[y][x] = true;
+          }
+        }
+      }
+    }
+
+    setGrid(newGrid);
+    setLiveCells(getLiveCells(rows, cols, newGrid));
+    setGeneration((prevGen) => prevGen + 1);
+  };
+
+  const makeNewGenerations = (grid, cols, rows) => {
+    const newGrid = makeEmptyGrid(rows, cols);
+
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        let neighbors = getNeighborCount(grid, x, y, cols, rows);
+
+        if (grid[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newGrid[y][x] = true;
+          } else {
+            newGrid[y][x] = false;
+          }
+        } else {
+          if (!grid[y][x] && neighbors === 3) {
+            newGrid[y][x] = true;
+          }
+        }
+      }
+    }
+
+    setGrid(newGrid);
+    setLiveCells(getLiveCells(rows, cols, newGrid));
+    setGeneration((prevGen) => prevGen + 1);
+
+    const id = setTimeout(() => {
+      makeNewGenerations(newGrid, cols, rows);
+    }, 1000);
+
+    setTimeoutId(id);
+  };
+
   return (
     <section>
       <h3>Controls</h3>
+      <div>
+        {isRunning ? (
+          <button onClick={stop}>Stop</button>
+        ) : (
+          <button onClick={start}>Start</button>
+        )}
+        <button onClick={next}>Next</button>
+      </div>
     </section>
   );
 }
